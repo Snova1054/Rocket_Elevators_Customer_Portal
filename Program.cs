40d31b31620c464e5cf902bf45a9ca1563d80d21
Snova1054 <istar.dark@gmail.com>
2021-12-13 18:36:48 -0500

    added api call on program.cs

--- Program.cs
+++ Program.cs
@@ -6,13 +6,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace Rocket_Elevators_Customer_Portal
 {
     public class Program
     {
-        public static void Main(string[] args)
+        private static async Task Hello()
         {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync("http://numbersapi.com/12/math");
+
+            var msg = await stringTask;
+            Console.WriteLine(msg);
+        }
+        static async Task Main(string[] args)
+        {
+            await Hello();
             CreateHostBuilder(args).Build().Run();
         }
 
