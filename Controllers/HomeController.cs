54b9ce224889e7a0f6c0c245699b7d55b17d6277
Daniel Gallardo Ishii <dgallardo@edu.cegepgarneau.ca>
2021-12-17 20:22:04 -0500

    intervention post perfectly working

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.Logging;
 using Rocket_Elevators_Customer_Portal.Models;
 using System;
+using System.Net;
+using System.IO;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -20,8 +22,6 @@ namespace Rocket_Elevators_Customer_Portal.Controllers
     public class HomeController : Controller
     {
         private static readonly HttpClient client = new HttpClient();
-        //SignInManager<IdentityUser> SignInManager;
-        //UserManager<IdentityUser> UserManager;
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -40,34 +40,31 @@ namespace Rocket_Elevators_Customer_Portal.Controllers
         }
 
         public IActionResult Dashboard(string author, string start_date, string end_date, string result, string report, string status, string customer_id, string building_id, string battery_id, string column_id, string elevator_id, string employee_id)
-        //public IActionResult Dashboard(string interventionString)
         {
-            //Console.WriteLine("allo");
-            //Console.WriteLine(customer_id);
-            //Console.WriteLine(building_id);
-            //Console.WriteLine(battery_id);
-            //Console.WriteLine(column_id);
-            //Console.WriteLine(elevator_id);
-
             Intervention clientIntervention = new Intervention() {author = author, start_date = null, end_date = null, result = result, report = report, status = status, customer_id = Convert.ToInt32(customer_id), building_id = Convert.ToInt32(building_id), battery_id = Convert.ToInt32(battery_id), column_id = Convert.ToInt32(column_id), elevator_id = Convert.ToInt32(elevator_id), employee_id = null};
-            Console.WriteLine(clientIntervention.author);
-            //my own cors policy
-            //var values = new Dictionary<string, string>
-            //{
-            //    { "thing1", "hello" },
-            //    { "thing2", "world" }
-            //};
 
-            //var content = new FormUrlEncodedContent(values);
-            string Json = JsonSerializer.Serialize(clientIntervention);
+            if (clientIntervention.elevator_id != 0)
+            {
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://hidden-woodland-68127.herokuapp.com/api/interventions");
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
 
-            client.PostAsJsonAsync("https://hidden-woodland-68127.herokuapp.com/api/interventions", Json);
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    string json = JsonSerializer.Serialize(clientIntervention);
+
+
+                    streamWriter.Write(json);
+                }
+
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var hello = streamReader.ReadToEnd();
+                }
+
+            }
             return View();
-        }
-
-        public void postIntervention(string intervention)
-        {
-            Console.WriteLine("allo {0}", intervention);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
